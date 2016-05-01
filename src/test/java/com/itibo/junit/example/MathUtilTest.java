f8b37f5a5d217c1938b233402ba8e718f3b0b338
UnionOne <hax2033@gmail.com>
2016-05-01 22:37:36 +0300

    Logging added in junit test classes

--- src/test/java/com/itibo/junit/example/MathUtilTest.java
+++ src/test/java/com/itibo/junit/example/MathUtilTest.java
@@ -1,53 +1,55 @@
 package com.itibo.junit.example;
 
+import org.apache.log4j.Logger;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class MathUtilTest {
+    private final static Logger logger = Logger.getLogger(MathUtilTest.class);
 
     private MathUtil mathUtil;
 
     @Before
     public void setUp(){
-        System.out.println("\n********** SETTING UP **********");
+        logger.info("********* SETTING UP *********");
         mathUtil = new MathUtil();
-        System.out.println("\n********** SETTING UP SUCCESSFUL **********");
+        logger.info("********* SETTING UP SUCCESSFUL *********");
     }
 
     @Test
     public void sumTest(){
-        System.out.println("\n********** SUM TEST **********");
+        logger.info("********* SUM TEST *********");
         assertEquals(mathUtil.sum(2,2),4);
     }
 
     @Test
     public void multiplyTest(){
-        System.out.println("\n********** MULTIPLY TEST **********");
+        logger.info("********* MULTIPLY TEST *********");
         assertTrue(mathUtil.multiply(3,2) == 6);
     }
 
     @Test
     public void residueTest(){
-        System.out.println("\n********** RESIDUE TEST **********");
-        assertEquals(mathUtil.residue(2,4),2);
+        logger.info("********* RESIDUE TEST *********");
+        assertEquals(mathUtil.residue(2,4),3);
     }
 
     @Test
     public void factorialTest(){
-        System.out.println("\n********** FACTORIAL TEST **********");
+        logger.info("********* FACTORIAL TEST *********");
         assertEquals(mathUtil.factorial(2),2);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void factorialNegativeNumberTest(){
-        System.out.println("\n********** FACTORIAL WITH NEGATIVE TEST **********");
+        logger.info("********* FACTORIAL WITH NEGATIVE TEST *********");
         mathUtil.factorial(-5);
     }
 
     @After
     public void down(){
-        System.out.println("\n********** SETTING DOWN **********");
+        logger.info("********* SETTING DOWN *********");
         mathUtil = null;
-        System.out.println("\n********** SETTING DOWN SUCCESSFUL **********");
+        logger.info("********* SETTING DOWN SUCCESSFUL *********");
     }
 }
--- src/test/java/com/itibo/junit/example/StringUtilTest.java
+++ src/test/java/com/itibo/junit/example/StringUtilTest.java
@@ -1,5 +1,6 @@
 package com.itibo.junit.example;
 
+import org.apache.log4j.Logger;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -7,44 +8,45 @@ import static org.junit.Assert.*;
  * Created by vlad on 28.04.16.
  */
 public class StringUtilTest {
+    private final static Logger logger = Logger.getLogger(StringUtilTest.class);
 
     private static StringUtil stringUtil;
 
     @BeforeClass
     public static void setUp(){
-        System.out.println("\n********** SETTING UP **********");
+        logger.info("********* SETTING UP *********");
         stringUtil = new StringUtil();
-        System.out.println("\n********** SETTING UP SUCCESSFUL **********");
+        logger.info("********* SETTING UP SUCCESSFUL *********");
     }
 
     @Test
     public void concatTest(){
-        System.out.println("\n********** CONCAT TEST **********");
+        logger.info("********* CONCAT TEST *********");
         assertEquals(stringUtil.concat("vl","ad"),"vlad");
     }
 
     @Test
     public void deleteCharTest(){
-        System.out.println("\n********** DELETE CHAR TEST **********");
+        logger.info("********* DELETE CHAR TEST *********");
         assertEquals(stringUtil.deleteChar("vlad",1),"vad");
     }
 
     @Test(expected = StringIndexOutOfBoundsException.class)
     public void deleteCharIllegalArgTest(){
-        System.out.println("\n********** DELETE CHAR ILLEGAL ARGS TEST **********");
+        logger.info("********* DELETE CHAR ILLEGAL ARGS TEST *********");
         assertEquals(stringUtil.deleteChar("vlad",4),"vad");
     }
 
     @Test(expected = NullPointerException.class)
     public void deleteCharNullTest(){
-        System.out.println("\n********** DELETE CHAR NULL POINTER TEST **********");
+        logger.info("********* DELETE CHAR NULL POINTER TEST *********");
         assertEquals(stringUtil.deleteChar(null,1),"vad");
     }
 
     @AfterClass
     public static void down(){
-        System.out.println("\n********** SETTING DOWN **********");
+        logger.info("********* SETTING DOWN *********");
         stringUtil = null;
-        System.out.println("\n********** SETTING DOWN SUCCESSFUL **********");
+        logger.info("********* SETTING DOWN SUCCESSFUL *********");
     }
 }
